f2c4ea48339e1dff0712fe9c4b466bd9ac5136d1
DatNgo <ngominhdat110115@gmail.com>
2022-10-16 22:00:55 +0700

    add category image

--- API/Entities/Category.cs
+++ API/Entities/Category.cs
@@ -9,6 +9,7 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; } = "";
+        public string Image { get; set; } = "";
         public List<Product> Products { get; set; } = new List<Product>();
     }
 }
