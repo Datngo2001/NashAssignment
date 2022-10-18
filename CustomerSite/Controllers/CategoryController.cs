7fd54de44061acb4c38f85f8c3e7655cb0f05fe1
DatNgo <ngominhdat110115@gmail.com>
2022-10-18 13:28:43 +0700

    setting product category page

---
+++ CustomerSite/Controllers/CategoryController.cs
@@ -0,0 +1,30 @@
+﻿using CustomerSite.Interfaces;
+using CustomerSite.Models;
+using CustomerSite.Views.Shared.Components.ProductCardList;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CustomerSite.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            this.categoryService = categoryService;
+        }
+
+        public async Task<IActionResult> Index(int? id)
+        {
+            ViewData["name"] = id;
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
