045c4d5068ff525669b1c99e302241a3b54ab575
DatNgo <ngominhdat110115@gmail.com>
2022-10-30 12:57:00 +0700

    init admin route

--- AdminSite/src/layout/LoginLayout.jsx
+++ AdminSite/src/layout/LoginLayout.jsx
@@ -1,8 +1,13 @@
 import React from "react";
-import wallpager from "./assets/login_page.webp";
+import wallpager from "../assets/login_page.webp";
+import LoginRoute from "../routes/LoginRoute";
 
 function LoginLayout() {
-  return <div>LoginLayout</div>;
+  return (
+    <div style={{ backGroundImage: wallpager }}>
+      <LoginRoute />
+    </div>
+  );
 }
 
 export default LoginLayout;
--- AdminSite/src/routes/LoginRoute.jsx
+++ AdminSite/src/routes/LoginRoute.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import LoginPage from "../pages/Login/LoginPage";
+import CallbackPage from "../pages/Login/CallbackPage";
+import { Route, Routes } from "react-router";
 
 function LoginRoute() {
-  return <div>LoginRoute</div>;
+  return (
+    <Routes>
+      <Route index element={<LoginPage />} />
+      <Route path="/callback" element={<CallbackPage />} />
+    </Routes>
+  );
 }
 
 export default LoginRoute;
