0e2fd56392a096540938881a66ded80e154ef339
DatNgo <ngominhdat110115@gmail.com>
2022-11-02 09:15:57 +0700

    fix import

--- admin-site/src/store/reducer/category/categoryActions.js
+++ admin-site/src/store/reducer/category/categoryActions.js
@@ -1,10 +1,10 @@
 import { put } from 'redux-saga/effects'
-import { getCategorys, postCategory } from '../../../api/product'
-import { CREATE_CATEGORY_FAILURE, CREATE_CATEGORY_SUCCESS, SEARCH_CATEGORY_FAILURE, SEARCH_CATEGORY_SUCCESS } from './productActionTypes'
+import { getCategories, postCategory } from '../../../api/category'
+import { CREATE_CATEGORY_FAILURE, CREATE_CATEGORY_SUCCESS, SEARCH_CATEGORY_FAILURE, SEARCH_CATEGORY_SUCCESS } from './categoryActionTypes'
 
 export function* searchCategory({ payload }) {
     try {
-        var res = yield getCategorys(payload.query, payload.page, payload.limit)
+        var res = yield getCategories(payload.query, payload.page, payload.limit)
         yield put({
             type: SEARCH_CATEGORY_SUCCESS,
             payload: res.data
--- admin-site/src/store/reducer/category/categorySaga.js
+++ admin-site/src/store/reducer/category/categorySaga.js
@@ -1,6 +1,6 @@
 import { takeLatest } from "redux-saga/effects";
-import { CREATE_CATEGORY_REQUEST, SEARCH_CATEGORY_REQUEST } from "./productActionTypes";
-import { createCategory, searchCategory } from "./productActions";
+import { CREATE_CATEGORY_REQUEST, SEARCH_CATEGORY_REQUEST } from "./categoryActionTypes";
+import { createCategory, searchCategory } from "./categoryActions";
 
 export default function* watchCategoryAction() {
     yield takeLatest(SEARCH_CATEGORY_REQUEST, searchCategory)
