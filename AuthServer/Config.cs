e3a01cbb28b87c438c22b566b579d329339b9081
DatNgo <ngominhdat110115@gmail.com>
2022-11-08 23:37:35 +0700

    add to app setting

--- AuthServer/Config.cs
+++ AuthServer/Config.cs
@@ -26,18 +26,6 @@ namespace AuthServer
         public static IEnumerable<Client> Clients =>
         new Client[]
         {
-                // m2m client credentials flow client
-                new Client
-                {
-                    ClientId = "m2m.client",
-                    ClientName = "Client Credentials Client",
-
-                    AllowedGrantTypes = GrantTypes.ClientCredentials,
-                    ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
-
-                    AllowedScopes = { "AssignmentAPI" }
-                },
-
                 // interactive client using code flow + pkce
                 new Client
                 {
