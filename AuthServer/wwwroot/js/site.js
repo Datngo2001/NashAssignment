8a8ebfb3564111b5037996740af0f9add0d2966f
DatNgo <ngominhdat110115@gmail.com>
2022-11-08 14:45:11 +0700

    init AuthServer Project

--- AuthServer/wwwroot/js/site.js
+++ AuthServer/wwwroot/js/site.js
@@ -2,5 +2,3 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-const BASE_URL = "https://localhost:5002/"
-const API_URL = "https://localhost:5003/api/"
