84501aa9ec0deb6bd4734ad861edc0baa90dc316
DatNgo <ngominhdat110115@gmail.com>
2023-05-15 00:49:48 +0700

    deploy

--- admin-site/src/config.js
+++ admin-site/src/config.js
@@ -1,9 +1,13 @@
-export const { NODE_ENV } = process.env;
+export const { NODE_ENV, APP_API_URL } = process.env;
 
 let apiurl;
 if (NODE_ENV === 'development') {
   apiurl = 'https://localhost:5003/api';
 } else {
-  apiurl = '';
+  if (APP_API_URL) {
+    apiurl = APP_API_URL;
+  } else {
+    apiurl = 'https://group01-softwaretesting-api.azurewebsites.net/api';
+  }
 }
 export const API_URL = apiurl;
